ef288f287f042bc100467f8a34e6ee1e287ca98e
KARTIK BALIYAN <kartikbaliyan00004@gmail.com>
2026-02-12 23:08:25 +0530

    Time: 0 ms (100.00%) | Memory: 13.7 MB (42.45%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int first = 0;
+        int last = nums.size() - 1;
+        int mid = (first + last) / 2;
+        while (first <= last) {
+            if (nums[mid] == target) {
+                return mid;
+            } else {
+                if (nums[mid] >= target) {
+                    last = mid - 1;
+                } else {
+                    first = mid + 1;
+                }
+            }
+            mid = (first + last) / 2;
+        }
+        
+        return first;
+    }
+};

ace39e63c999b57773bef0b989d407beb547e80b
KARTIK BALIYAN <kartikbaliyan00004@gmail.com>
2026-02-14 00:13:36 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (81.25%) - LeetSync

---
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        
+        int sum =0;
+        for(int i=0;i< nums.size() ;i++){
+            nums[i]= sum + nums[i];
+            sum = nums[i];
+        }
+        return nums;
+
+    }
+};

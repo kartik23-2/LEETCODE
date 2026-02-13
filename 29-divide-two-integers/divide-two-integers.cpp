777659f234d5a1ded6af0dbdbfe754131eee04f5
KARTIK BALIYAN <kartikbaliyan00004@gmail.com>
2026-02-14 00:23:02 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (38.28%) - LeetSync

---
+++ 29-divide-two-integers/divide-two-integers.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    long long divide(int dividend, int divisor) {
+        if (dividend == INT_MIN && divisor == -1){
+            return INT_MAX;
+        }
+        return (long long)dividend / divisor;
+
+
+    }
+};

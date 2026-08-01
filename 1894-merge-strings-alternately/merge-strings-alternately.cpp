cf5ec07da01e41babf0a6492d889ad0405c8df17
KARTIK BALIYAN <kartikbaliyan00004@gmail.com>
2026-08-01 15:25:44 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (31.70%) - LeetSync

---
+++ 1894-merge-strings-alternately/merge-strings-alternately.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        int i=0,j= 0;
+        string s;
+        while(i<word1.size() || j<word2.size()){
+            if(i<word1.size()){
+                s.push_back(word1[i]);
+                i++;
+            }
+            if(j<word2.size()){
+                s.push_back(word2[j]);
+                j++;
+            }
+        }
+        return s;
+    }
+};

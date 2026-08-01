33c8f2c0e17693bafff168db399baeec98fe80f9
KARTIK BALIYAN <kartikbaliyan00004@gmail.com>
2026-08-01 16:49:59 +0530

    Time: 0 ms (100.00%) | Memory: 29.7 MB (47.04%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -1,12 +1,23 @@
+// class Solution {
+// public:
+//     void rotate(vector<int>& nums, int k) {
+//         int n =nums.size();
+//         k = k%n;
+//         for(int i=0;i<n;i++){
+//             nums.push_back(nums[i]);
+//         }
+//         nums.erase(nums.begin() + 2*n-k, nums.end());
+//         nums.erase(nums.begin() + 0, nums.begin() + n-k);
+//     }
+// };
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        int n =nums.size();
-        k = k%n;
-        for(int i=0;i<n;i++){
-            nums.push_back(nums[i]);
-        }
-        nums.erase(nums.begin() + 2*n-k, nums.end());
-        nums.erase(nums.begin() + 0, nums.begin() + n-k);
+        int n = nums.size();
+        k %= n;
+
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin() + k);
+        reverse(nums.begin() + k, nums.end());
     }
 };

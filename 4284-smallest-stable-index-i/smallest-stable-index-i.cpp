4c7942a5d9b051a2608fe750d969450305e70061
KARTIK BALIYAN <kartikbaliyan00004@gmail.com>
2026-09-04 16:52:57 +0530

    Time: 0 ms (100.00%) | Memory: 31.2 MB (56.73%) - LeetSync

---
+++ 4284-smallest-stable-index-i/smallest-stable-index-i.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+        vector<int> suffix(nums.size(),nums[nums.size()-1]);
+        int minc= suffix[nums.size()-1];
+        for(int i =nums.size()-1;i>=0;i--){
+           minc=min(minc,nums[i]);
+           suffix[i]=minc;
+        }
+        int prefix=nums[0];
+        
+        int res=-1;
+        for(int i=0;i<nums.size();i++){
+            prefix=max(prefix,nums[i]);
+            if((prefix-suffix[i])<=k){
+             return i;
+            }
+        }
+        return -1;
+    }
+};

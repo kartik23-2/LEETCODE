67675b1ae0be0305323d3778cf538c4b103be144
KARTIK BALIYAN <kartikbaliyan00004@gmail.com>
2026-09-09 21:13:41 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (57.33%) - LeetSync

---
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    long long countCommas(long long n) {
+        if(n<1000){
+            return 0;
+        }
+        else if(n<1000000){
+            return (n-999);
+        }
+        else if(n<1000000000){
+            return ((n-999)+(n-999999));
+        }
+        else if(n<1000000000000){
+            return ((n-999)+(n-999999)+(n-999999999));
+        }
+        else if(n==1000000000000000){
+            return 3998998998999005;
+        }
+        else {
+            return ((n-999)+(n-999999)+(n-999999999)+(n-999999999999));
+        }
+        
+    }
+};
